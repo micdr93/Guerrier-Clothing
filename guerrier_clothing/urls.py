b3867c5140bf31dd2d425b21e33ee17f995f240f
Michael Drew <michaeldrew_@outlook.com>
2025-01-11 15:08:10 +0000

    updated urls

--- guerrier_clothing/urls.py
+++ guerrier_clothing/urls.py
@@ -18,9 +18,19 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import handler404
+from .views import handler500
+
+handler404 = 'performance_footwear.views.handler404'
+handler500 = 'performance_footwear.views.handler500'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
+    path('products/', include('products.urls')),
+    path('bag/', include('bag.urls')),
+    path('checkout/', include('checkout.urls')),
+    path('profile/', include('profiles.urls')),
+    path('wishlist/', include('wishlist.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+    path('summernote/', include('django_summernote.urls')),
+    path("privacy_policy", views.privacy_policy, name="privacy_policy"),
+    path("returns", views.returns, name="returns"),
+    path("contact", views.contact, name="contact"),
 ]
