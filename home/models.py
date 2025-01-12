9064359f35bb5585b1129fa6076c11ec263f1ffc
Michael Drew <michaeldrew_@outlook.com>
2025-01-12 01:29:01 +0000

    made migrations and id issues on product.json

--- home/models.py
+++ home/models.py
@@ -11,9 +11,9 @@ class Contact(models.Model):
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
-    description = models.TextField()
+    description = models.CharField(max_length=255, default="Default description")
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    stock = models.PositiveIntegerField()
+    stock = models.PositiveIntegerField(default=1)
 
     def __str__(self):
         return self.name
@@ -21,7 +21,7 @@ class Product(models.Model):
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
-    description = models.TextField()
+    description = models.CharField(max_length=255, default="Default description")
 
     def __str__(self):
         return self.name
--- products/models.py
+++ products/models.py
@@ -20,11 +20,13 @@ class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
-    description = models.TextField()
+    description = models.CharField(max_length=255, default="Default description")
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
+
+
 
     def __str__(self):
         return self.name
