f16a9339b32e2069673c7ca88d0b100d44a3cc8a
Michael Drewe <michaeldrew_@outlook.com>
2025-03-05 00:44:15 +0000

    fix:  fixed contact form issue

--- home/forms.py
+++ home/forms.py
@@ -1,26 +1,26 @@
 from django import forms
-from .models import Contact
-from .models import Product
-from .models import Category 
-from .models import NewsletterSubscription
-from .models import Contact
-
+from .models import Contact, Product, Category, NewsletterSubscription
 
 class ContactForm(forms.ModelForm):
     class Meta:
         model = Contact  
-        fields = ['name', 'email', 'message'] 
-        
+        fields = ['name', 'email', 'subject', 'message']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'subject': forms.TextInput(attrs={'class': 'form-control'}),
+            'message': forms.Textarea(attrs={'class': 'form-control', 'rows': 5}),
+        }
+
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product 
         fields = ['name', 'description', 'price', 'stock']
 
 class CategoryForm(forms.ModelForm):
-     class Meta:
+    class Meta:
         model = Category
         fields = ['name', 'description']
-
 
 class NewsletterForm(forms.ModelForm):
     class Meta:
@@ -28,15 +28,4 @@ class NewsletterForm(forms.ModelForm):
         fields = ['email']
         widgets = {
             'email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Enter your email address'})
-        }
-
-class ContactForm(forms.ModelForm):
-    class Meta:
-        model = Contact
-        fields = ['name', 'email', 'subject', 'message']
-        widgets = {
-            'name': forms.TextInput(attrs={'class': 'form-control'}),
-            'email': forms.EmailInput(attrs={'class': 'form-control'}),
-            'subject': forms.TextInput(attrs={'class': 'form-control'}),
-            'message': forms.Textarea(attrs={'class': 'form-control', 'rows': 5}),
         }
--- home/models.py
+++ home/models.py
@@ -1,14 +1,6 @@
 
 from django.db import models
 
-class Contact(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.EmailField()
-    message = models.TextField()
-
-    def __str__(self):
-        return self.name
-
 class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=255, default="Default description")
