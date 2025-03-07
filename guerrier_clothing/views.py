c80be95396838020644b774811f6a89b7a808c97
Michael Drewe <michaeldrew_@outlook.com>
2025-03-07 09:57:12 +0000

    update index, views, settings, and base.html

--- guerrier_clothing/views.py
+++ guerrier_clothing/views.py
@@ -11,10 +11,10 @@ def index(request):
 
 
 def handler404(request, exception):
-    return render(request, '404.html', status=404)
+    return render(request, 'errors/404.html', status=404)
 
 def handler500(request):
-    return render(request, '500.html', status=500)
+    return render(request,'errors/500.html', status=500)
 
 
 def products_view(request):
