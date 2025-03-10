0189a9a98982181870bbaff41f6051f48f93739c
Michael Drewe <michaeldrew_@outlook.com>
2025-03-10 19:17:52 +0000

    removed staticsfiles folder

--- staticfiles/js/scripts.048d2075dd48.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const searchToggler = document.getElementById('search-toggler');
-    const mobileSearch = document.getElementById('mobile-search');
-  
-    searchToggler.addEventListener('click', () => {
-      const bsCollapse = new bootstrap.Collapse(mobileSearch, { toggle: true });
-    });
-  });
