ec9fa739f8d6704a836e08a41e54178aa8267644
Michael Drewe <michaeldrew_@outlook.com>
2025-03-16 20:58:02 +0000

    debugging directory issues after djangocrash

--- reviews/migrations/0001_initial.py
+++ reviews/migrations/0001_initial.py
@@ -12,56 +12,30 @@ class Migration(migrations.Migration):
 
     dependencies = [
         ("products", "0006_alter_review_options_category_description_and_more"),
-        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
         migrations.CreateModel(
             name="Review",
             fields=[
-                (
-                    "id",
-                    models.BigAutoField(
-                        auto_created=True,
-                        primary_key=True,
-                        serialize=False,
-                        verbose_name="ID",
-                    ),
-                ),
+                ("id", models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name="ID")),
                 ("title", models.CharField(max_length=100)),
                 ("review", models.TextField(max_length=1500)),
-                (
-                    "rating",
-                    models.IntegerField(
-                        validators=[
-                            django.core.validators.MinValueValidator(
-                                1, "Rating must be at least 1"
-                            ),
-                            django.core.validators.MaxValueValidator(
-                                5, "Rating cannot exceed 5"
-                            ),
-                        ]
-                    ),
-                ),
+                ("rating", models.IntegerField(validators=[
+                    django.core.validators.MinValueValidator(1, "Rating must be at least 1"),
+                    django.core.validators.MaxValueValidator(5, "Rating cannot exceed 5"),
+                ])),
                 ("verified_purchase", models.BooleanField(default=False)),
                 ("helpful_votes", models.PositiveIntegerField(default=0)),
                 ("created_on", models.DateTimeField(auto_now_add=True)),
                 ("updated_on", models.DateTimeField(auto_now=True)),
                 (
                     "product",
-                    models.ForeignKey(
-                        on_delete=django.db.models.deletion.CASCADE,
-                        related_name="reviews_new",
-                        to="products.product",
-                    ),
+                    models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name="reviews_new", to="products.product"),
                 ),
                 (
                     "user",
-                    models.ForeignKey(
-                        on_delete=django.db.models.deletion.CASCADE,
-                        related_name="product_reviews_new",
-                        to=settings.AUTH_USER_MODEL,
-                    ),
+                    models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name="product_reviews_new", to=settings.AUTH_USER_MODEL),
                 ),
             ],
             options={
@@ -71,8 +45,6 @@ class Migration(migrations.Migration):
         ),
         migrations.AddConstraint(
             model_name="review",
-            constraint=models.UniqueConstraint(
-                fields=("user", "product"), name="unique_review_per_user_new"
-            ),
+            constraint=models.UniqueConstraint(fields=("user", "product"), name="unique_review_per_user_new"),
         ),
     ]
